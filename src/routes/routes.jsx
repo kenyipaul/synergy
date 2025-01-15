b293a4d8bf2ba62d44e6bbb1d4bf23d60781001e
Kenyi Paul <kenyipaul69@gmail.com>
2025-01-15 17:51:15 +0200

    Switched backend url to server url

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,7 @@
-export const BackendHost = "http://localhost:3303"
-// export const BackendHost = "https://synergy-backend-u63k.onrender.com"
+// import { useEffect } from "react"
+// import { io } from "socket.io-client"
+// export const BackendHost = "http://localhost:3303"
+export const BackendHost = "https://synergy-backend-u63k.onrender.com"
 
 export const loginRoute = `${BackendHost}/api/login/`
 export const signupRoute = `${BackendHost}/api/signup/`
@@ -8,3 +10,15 @@ export const eventRoute = `${BackendHost}/api/event/`
 export const eventsRoute = `${BackendHost}/api/events/`
 export const communityRoute = `${BackendHost}/api/community`
 export const postRoute = `${BackendHost}/api/post`
+
+// const socket = useRef();
+
+// useEffect(() => {
+//     socket.current = io();
+// }, [])
+
+// const login = () => {
+
+//     socket.current.emit("addUser", id)
+
+// }

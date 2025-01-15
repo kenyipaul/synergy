a02c70d542f60725073464b1d6a3193afef1b025
Kenyi Paul <kenyipaul69@gmail.com>
2025-01-15 17:51:15 +0200

    Switched backend url to server url

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,5 @@
-export const BackendHost = "http://localhost:3303"
-// export const BackendHost = "https://synergy-backend-u63k.onrender.com"
+// export const BackendHost = "http://localhost:3303"
+export const BackendHost = "https://synergy-backend-u63k.onrender.com"
 
 export const loginRoute = `${BackendHost}/api/login/`
 export const signupRoute = `${BackendHost}/api/signup/`

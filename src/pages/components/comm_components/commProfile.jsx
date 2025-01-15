d0b017ba15e6b4b6dafd48835b502479a4052a72
Kenyi Paul <kenyipaul69@gmail.com>
2025-01-15 17:44:12 +0200

    -Fixed issues with login, signup and create community not showing progres

--- src/pages/components/comm_components/commProfile.jsx
+++ src/pages/components/comm_components/commProfile.jsx
@@ -12,8 +12,10 @@ export default function CommunityProfile() {
     const location = useLocation();
     const [posts, setPosts] = useState([])
     const [response, setResponse] = useState([])
+    const [updater, setUpdater] = useState(false)
     const [currentTab, setCurrentTab] = useState(1);
     const [pageLoading, setPageLoading] = useState(true)
+    const [optionMenuState, setOptionMenuState] = useState(false)
     const [postCreatorState, setPostCreatorState] = useContext(PostCreatorContext)
 
     const tmp = location.pathname.split("/")
@@ -30,7 +32,7 @@ export default function CommunityProfile() {
             setPageLoading(false)
         })
 
-    }, [community_id])
+    }, [community_id, updater])
 
 
     useEffect(() => {
@@ -53,12 +55,29 @@ export default function CommunityProfile() {
             method: "POST",
             url: `${communityRoute}/join`,
             data: {
-                community_id: props.data._id,
+                community_id,
                 user_id: authorizedState.user.id
             }
         }).then((response) => {
             alert(response.data.msg)
+            setUpdater(!updater)
         })
+    }
+    
+    const exitComm = () => {
+        if (confirm("Are you sure you want to leave this community?")) {
+            Axios({
+                method: "POST",
+                url: `${BackendHost}/api/community/leave`,
+                data: {
+                    community_id,
+                    user_id: authorizedState.user.id
+                }
+            }).then((response) => {
+                alert(response.data.msg)
+                setUpdater(!updater)
+            })
+        }
     }
 
     return (
@@ -81,14 +100,21 @@ export default function CommunityProfile() {
                         }}></div>
                             <div>
                                 <h1>{response.community_name}</h1>
-                                {/* <p>{response.community_members && response.community_members.length} members</p> */}
                             </div>
                         </div>
 
                         <div className="buttons">
                             {/* <button>Create Post</button> */}
-                            { response.community_members && !response.community_members.includes(authorizedState.user.id) && <button onClick={join}>Join</button> }
+                            { response.community_members && !response.community_members.includes(authorizedState.user.id) && <button className="joinBtn" onClick={join}>Join</button> }
                             { response.community_members && response.community_members.includes(authorizedState.user.id) && <button onClick={() => setPostCreatorState({id: response._id, state: true})}>Create Post</button> }
+                                <button className="optionBtn" onClick={() => setOptionMenuState(!optionMenuState)}>
+                                <svg width="1.5rem" height="1.5rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 12C7 13.1046 6.10457 14 5 14C3.89543 14 3 13.1046 3 12C3 10.8954 3.89543 10 5 10C6.10457 10 7 10.8954 7 12Z" fill="currentColor"></path> <path d="M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z" fill="currentColor"></path> <path d="M21 12C21 13.1046 20.1046 14 19 14C17.8954 14 17 13.1046 17 12C17 10.8954 17.8954 10 19 10C20.1046 10 21 10.8954 21 12Z" fill="currentColor"></path> </g></svg>
+                                { optionMenuState ?
+                                <div className="option-menu">
+                                    <li onClick={exitComm}>Leave Community</li>
+                                </div>
+                                : <></> }
+                            </button> 
                         </div>
                     </section>
                 </div>
